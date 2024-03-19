424084370b9315546e889456b3515fe22661c2e8
Hegerhog <phamgiabao1682003@gmail.com>
2024-03-20 00:17:52 +0700

    First commit

--- src/java/Entity/Constants.java
+++ src/java/Entity/Constants.java
@@ -1,26 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Entity;
 
 import java.io.Serializable;
 
 /**
- *
- * @author ACER
+ * Constants class to store application constants.
+ * Replace hardcoded values with environment variables.
  */
 public class Constants implements Serializable {
 
-    public static String GOOGLE_CLIENT_ID = "792958604853-t1da5rg9q5fblc145j7a30814recg0js.apps.googleusercontent.com";
+    // Define environment variable names
+    private static final String ENV_GOOGLE_CLIENT_ID = "GOOGLE_CLIENT_ID";
+    private static final String ENV_GOOGLE_CLIENT_SECRET = "GOOGLE_CLIENT_SECRET";
 
-    public static String GOOGLE_CLIENT_SECRET = "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";
+    // Get Google OAuth client ID from environment variable
+    public static String GOOGLE_CLIENT_ID = System.getenv(ENV_GOOGLE_CLIENT_ID);
 
+    // Get Google OAuth client secret from environment variable
+    public static String GOOGLE_CLIENT_SECRET = System.getenv(ENV_GOOGLE_CLIENT_SECRET);
+
+    // Define other constants
     public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/QuizzesOnline/LoginGoogleURL";
-
     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
-
     public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
-
     public static String GOOGLE_GRANT_TYPE = "authorization_code";
 }
